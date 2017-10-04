991e825d9f69856a964c46cfd5a594bac1b62c46
Glen Maddern <glenmaddern@gmail.com>
2017-10-04 13:06:26 +1100

    simplified this couple of components

--- src/App.js
+++ src/App.js
@@ -2,27 +2,20 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-const Greeting = ({ greeting, onClick }) => (
-  <h1 className="App-title">
-    {greeting}
-    <button type="button" onClick={onClick}>Click me!</button>
-  </h1>
-)
-
 const AppHeader = ({ greeting, onClick }) => (
   <header className={`App-header`}>
-    <img src={logo} className="App-logo" alt="logo"/>
-    <Greeting greeting={greeting} onClick={onClick}/>
+    <img src={logo}
+         className="App-logo"
+         alt="logo"/>
+    <h1 className="App-title">
+      {greeting}
+      <button type="button"
+              onClick={onClick}
+              className="App-header-button">
+        ℹ️
+      </button>
+    </h1>
   </header>
-)
-
-const AppRender = ({ greeting, onClick }) => (
-  <div className="App">
-    <AppHeader greeting={greeting} onClick={onClick}/>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
 )
 
 class App extends React.Component {
@@ -34,27 +27,20 @@ class App extends React.Component {
     lang: 'en'
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        greeting: {
-          en: 'Welcome to React!!!',
-          emoji: '👋⚛️!!!'
-        }
-      })
-    }, 2000)
-  }
-
   handleClick = () => {
     this.setState({ lang: this.state.lang === 'en' ? 'emoji' : 'en' })
   }
 
   render() {
+    const { greeting, lang } = this.state
     return (
-      <AppRender
-        greeting={this.state.greeting[this.state.lang]}
-        onClick={this.handleClick}
-      />
+      <div className="App">
+        <AppHeader greeting={greeting[lang]}
+                   onClick={this.handleClick}/>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     )
   }
 }
