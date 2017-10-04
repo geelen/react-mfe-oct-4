812b492d4cf9da14de4601e68499c2df7c5b1cff
Glen Maddern <glenmaddern@gmail.com>
2017-10-04 12:51:01 +1100

    did some demos of props and state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
-import React, { Component } from 'react'
+import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-class App extends Component {
+const Greeting = ({ greeting, onClick }) => (
+  <h1 className="App-title">
+    {greeting}
+    <button type="button" onClick={onClick}>Click me!</button>
+  </h1>
+)
+
+const AppHeader = ({ greeting, onClick }) => (
+  <header className={`App-header`}>
+    <img src={logo} className="App-logo" alt="logo"/>
+    <Greeting greeting={greeting} onClick={onClick}/>
+  </header>
+)
+
+const AppRender = ({ greeting, onClick }) => (
+  <div className="App">
+    <AppHeader greeting={greeting} onClick={onClick}/>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+)
+
+class App extends React.Component {
+  state = {
+    greeting: {
+      en: 'Welcome to React',
+      emoji: '👋⚛️'
+    },
+    lang: 'en'
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        greeting: {
+          en: 'Welcome to React!!!',
+          emoji: '👋⚛️!!!'
+        }
+      })
+    }, 2000)
+  }
+
+  handleClick = () => {
+    this.setState({ lang: this.state.lang === 'en' ? 'emoji' : 'en' })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <AppRender
+        greeting={this.state.greeting[this.state.lang]}
+        onClick={this.handleClick}
+      />
     )
   }
 }
